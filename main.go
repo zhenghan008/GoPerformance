60892420340c91e8d9fef5e08bc9d5b8721f0b63
zhenghan008 <719470343@qq.com>
2022-05-25 16:20:16 +0800

    更新

--- main.go
+++ main.go
@@ -2,66 +2,59 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"os"
+	"os/signal"
+	"sync/atomic"
+	"syscall"
 )
 
 var (
-	i             = 0        // 全局变量i
-	ProducerMutex sync.Mutex // 生产者互斥锁 主要是为了不重复生成全局变量i
-	wg            sync.WaitGroup
+	i uint64 = 0 // 全局变量i
+
 )
 
 // Consumer 消费者
-func Consumer(num int, ch chan int) {
+func Consumer(num int, ch chan uint64) {
 	fmt.Println(fmt.Sprintf("%d 开始消费数据", num))
-	for {
-		if n, ok := <-ch; ok {
-			fmt.Println(fmt.Sprintf("由消费者%d消费的%d出队列", num, n))
-		} else {
-			break
-		}
-
+	for n := range ch {
+		fmt.Println(fmt.Sprintf("由消费者%d消费的%d出队列", num, n))
 	}
-	defer wg.Done()
 
 }
 
 // Producer 生产者
-func Producer(num int, ch chan int) {
+func Producer(num int, ch chan uint64) {
 
 	fmt.Println(fmt.Sprintf("%d 开始生产数据", num))
 	for {
 		if i > 1000 {
 			break
 		}
-		ProducerMutex.Lock()
 		ch <- i
 		fmt.Println(fmt.Sprintf("由生产者%d生产的%d进入队列", num, i))
-		i += 1
-		ProducerMutex.Unlock()
+		atomic.AddUint64(&i, 1)
 	}
-	defer wg.Done()
 
 }
 
 func main() {
-	ch := make(chan int, 3000)
-	//wg.Add(3)
-	for i := 1; i <= 3; i++ {
-		wg.Add(1)
+	ch := make(chan uint64, 1000)
+	for i := 0; i < 4; i++ {
 		go Producer(i, ch)
+	}
+	for i := 0; i < 3; i++ {
 		go Consumer(i, ch)
-	} // 三个消费者三个生产者
-	//defer func() {
-	//	if err := recover(); err != nil {
-	//		fmt.Println("err: ", err)
-	//	}
-	//}()
-	wg.Wait()
+	}
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("err: ", err)
+		}
+	}()
 	defer close(ch)
+	defer fmt.Println("关闭ch")
 
 	//阻塞main函数，ctr + C 退出
-	//sig := make(chan os.Signal, 1)
-	//signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	//fmt.Printf("quit (%v)\n", <-sig)
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Printf("quit (%v)\n", <-sig)
 }
